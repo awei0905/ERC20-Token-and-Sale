c4319aab2beba1007f3b8f5228fdda1b3d5afe0d
朱庭瑋 <awei0905@zhutingweide-MacBook-Pro.local>
2022-03-03 17:52:51 +0800

    增加 Token 數量。

--- client/src/App.js
+++ client/src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
   }
 
   handleBuyTokens = async () => {
-    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0],value: this.web3.utils.toWei("1","wei")});
+    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0],value: this.web3.utils.toWei("8787878787","wei")});
   }
 
   handleInputChange = (event) => {
